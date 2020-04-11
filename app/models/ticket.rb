be9b4bd9e2e758efb8d94a213e9a03fbb8f458c3
NataliaEspinola <naespinola@miuandes.cl>
2020-04-11 13:43:57 -0400

    Callbacks

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,11 +5,29 @@ class Ticket < ApplicationRecord
   validates :order, presence: true
 
   # TODO: complete the folowing
-  # before_xxxxx :update_stats
+  before_create :update_stats
+  after_destroy :destroy_stats
 
   private
-    def update_stats
-      es = self.ticket_type.event.event_stat
-      # TODO: complete in order to update event stats
+  def update_stats
+    es = self.ticket_type.event.event_stat
+    es.tickets_sold ||= 0
+    begin
+      if self.ticket_type.event.event_venue.capacity == es.tickets_sold
+        raise 'Error: tickets are sold out'
+      end
+      es.update(tickets_sold: es.tickets_sold + 1)
+      es.update(tickets_sold: es.attendance + 1)
+      puts 'Updated ticket_sold and attendance'
     end
+  end
+
+  def destroy_stats
+    es = self.ticket_type.event.event_stat
+    es.tickets_sold ||= 0
+    es = self.ticket_type.event.event_stat
+    es.update(tickets_sold: es.attendance - 1)
+    es.update(tickets_sold: es.tickets_sold - 1)
+    puts 'Destroyed ticket'
+  end
 end
